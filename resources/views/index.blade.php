c37d7e0c55a29700fe5fc974aff802da81bcd566
Polvere243 <danielercoli@gmail.com>
2024-03-08 01:02:12 +0100

    created header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{ $title ?? '' }} | Fumetti</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="Daniele Ercoli">
+        <meta name="description" content="esercizio">
+        <meta name="keyword" content="laravel, blade, scss">
+        <meta name="viewport" content="width= device_width, initial-scale= 1.0">
+        @vite('resources/js/app.js')
+    </head>
+    <body>
+    <header>
+            <figure>
+                <img src="../assets/assets-vue-dc-comics-1/img/dc-logo.png" alt="logo DC Comics" />
+            </figure>
+            <nav>
+                <ul>
+                    <li v-for="(link, i) in links" :key="i">
+                        <a :href="{{ route('index') }}"></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div class="jumbotron"></div>
+    </body>
 
+        
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// rotta per la pagina dei fumetti
 Route::get('/', function () {
-    return view('welcome');
-});
+    $title = 'Fumetti';
+    return view('index', compact('title'));
+})->name('index');
+
+// rotta per la pagina dei personaggi
+Route::get('/characters', function () {
+    $title = 'characters';
+    return view('characters', compact('characters') );
+})->name('characters');
+// rotta per la pagina dei film
+Route::get('/movies', function () {
+    $title = 'film';
+    return view('movies',compact('film'));
+})->name('movies');
+// rotta per la pagina dei prodotti per la tv
+Route::get('/tv', function () {
+    $title = 'tv';
+    return view('tv', compact('film'));
+})->name('tv');
