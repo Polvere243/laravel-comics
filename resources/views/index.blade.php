0eaeb10860aa2abb3714317bd4ee47ece15e11a2
Polvere243 <danielercoli@gmail.com>
2024-03-08 09:08:50 +0100

    done part of the homework

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>{{ $title ?? '' }} | Fumetti</title>
+        <title>{{ env('APP_NAME') }} | Fumetti</title>
         <meta charset="UTF-8">
         <meta name="author" content="Daniele Ercoli">
         <meta name="description" content="esercizio">
@@ -12,7 +12,7 @@
     <body>
     <header>
             <figure>
-                <img src="../assets/assets-vue-dc-comics-1/img/dc-logo.png" alt="logo DC Comics" />
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC Comics" />
             </figure>
             <nav>
                 <ul>
@@ -23,6 +23,261 @@
             </nav>
         </header>
         <div class="jumbotron"></div>
+        <main>
+            <section id="comics-list">
+                <div class="container">
+                    <button class="series">CURRENT SERIES</button>
+                    <div class="row">
+                        <div class="card" v-for="(comic, i) in comics" :key="i">
+                            <figure>
+                                <img :src="comic.thumb" :alt="comic.series" />
+                                <figcaption>
+                                    <h4></h4>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="btn-box">
+                        <button class="more">
+                            <h3>LOAD MORE</h3>
+                        </button>
+
+                    </div>
+
+                </div>
+            </section>
+        </main>
+        <footer>
+            <FooterBar>
+                <section id="shop-sections">
+                    <ul class="links-pic">
+                        <li>
+                            <a href="#">
+                                <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-digital-comics.png" alt="kindle">
+                                <h4>DIGITAL COMICS</h4>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-merchandise.png" alt="maglietta">
+                                <h4>DC MERCHANDISE</h4>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-shop-locator.png" alt="tessere">
+                                <h4>SUBSCRIPTION</h4>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../assets/assets-vue-dc-comics-1/img/buy-comics-subscriptions.png"
+                                    alt="icona della posizione">
+                                <h4>COMIC SHOP LOCATOR</h4>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img class="visa" src="../assets/assets-vue-dc-comics-1/img/buy-dc-power-visa.svg" alt="carta visa">
+                                <h4>DC POWER VISA</h4>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+            </FooterBar>
+            <section id="site-links">
+                <div class="lists-row">
+                    <div>
+                        <h5>DC COMICS</h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Characters
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Comics
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Movies
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    TV
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Games
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Videos
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    News
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>SHOP</h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Shop DC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Shop DC Collectibles
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h5>DC</h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Terms of Use
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Privacy policy (New)
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Ad Choices
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Advertising
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Jobs
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Subscriptions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Talent Workshops
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    CPSC Certificates
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Ratings
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Shop Help
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Contact Us
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>SITES</h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    DC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    MAD Magazine
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC Kids
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC Universe
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC Power Visa
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section id="button-links">
+                <nav class="lower-footer">
+                    <div>
+                        <button class="sign-up">SIGN-UP NOW!</button>
+                    </div>
+                    <div class="f-links">
+                        <h5>FOLLOW US</h5>
+                        <ul class="links-list">
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/assets-vue-dc-comics-1/img/footer-facebook.png"
+                                        alt="logo di facebook" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/assets-vue-dc-comics-1/img/footer-twitter.png" alt="logo di twitter" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/assets-vue-dc-comics-1/img/footer-youtube.png" alt="logo di youtube" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/assets-vue-dc-comics-1/img/footer-pinterest.png"
+                                        alt="logo di pinterest" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/assets-vue-dc-comics-1/img/footer-periscope.png"
+                                        alt="logo di periscope" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+
+            </section>
+        </footer>
     </body>
 
         
