81119b95473daa0d1cb5e56ef086316c81387b37
Polvere243 <danielercoli@gmail.com>
2024-03-09 14:44:12 +0100

    filled the partials and included them into the page

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,55 @@
+<body>
+    <header>
+        <div class="top-background">
+            <div class="top-page">
+                <span>DC POWER VISA&reg;</span>
+                <span>ADDITIONAL DC SITES<i class="fas fa-sort-down"></i></span>
+            </div>
+
+        </div>
+        <div class="header-content">
+            <figure>
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC Comics" />
+            </figure>
+            <nav>
+                <ul>
+                    <li>
+                        <a :href="{{ route('index') }}">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">COMICS</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">MOVIES</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">TV</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">GAMES</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">FANS</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">NEWS</a>
+                    </li>
+                    <li>
+                        <a :href="{{ route('index') }}">SHOP<i class="fas fa-sort-down"></i></a>
+                    </li>
+                </ul>
+            </nav>
+            <form action="">
+                <input type="text" value="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </form>
+        </div>
+        
+    </header>
+    <div class="jumbotron"></div>
