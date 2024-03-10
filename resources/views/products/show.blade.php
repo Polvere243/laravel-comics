abd0b2900d0ab037ebb5bf66f3baa61efc403b36
Polvere243 <danielercoli@gmail.com>
2024-03-10 10:02:07 +0100

    working on the page, still in progress

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,7 +9,28 @@
     </figure>
 </div>
 <section id="comic-detail">
-
+    <div class="detail-card">
+        <div class="info">
+            <h2>{{ $comic['title'] }}</h2>
+            <div class="comic-info">
+                <div>
+                    <h4>U.S. Price: $19,99</h4>
+                    <h4>AVAILABLE</h4>
+                </div>
+                <div>
+                    <h4>Chack Availability<i class="fas fa-sort-down"></i></h4>
+                </div>
+            </div>
+            <p>{{$comic['description']}}</p>
+        </div>
+        <div class="adv">
+            <h5>ADVERTISEMENT</h5>
+            <figure>
+                <img class="adv-picture" src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+            </figure>
+        </div>
+        
+    </div>
 </section>
 
 @endsection
