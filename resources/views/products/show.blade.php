628539b697e3af1e359f17bcb471617c91d5daf4
Polvere243 <danielercoli@gmail.com>
2024-03-10 18:08:01 +0100

    fixed the upper infos

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -13,12 +13,12 @@
         <div class="info">
             <h2>{{ $comic['title'] }}</h2>
             <div class="comic-info">
-                <div>
-                    <h4>U.S. Price: $19,99</h4>
-                    <h4>AVAILABLE</h4>
+                <div class="flex padding">
+                    <h4 class="light-green" >U.S. Price: <span class="white">$19.99</span></h4>
+                    <h4 class="light-green" >AVAILABLE</h4>
                 </div>
-                <div>
-                    <h4>Chack Availability<i class="fas fa-sort-down"></i></h4>
+                <div class="border white padding">
+                    <h4>Check Availability<i class="fas fa-sort-down"></i></h4>
                 </div>
             </div>
             <p>{{$comic['description']}}</p>
